2fd9b9244918d34419586a887493d65fd0fe2f14
Evan Lane <evanklane@yahoo.com>
2018-12-22 05:02:08 -0500

    added articles to home screen

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, Image, StyleSheet, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-navigation'
 
 export default class Home extends Component{
 
-  render(){
+    constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+    }
+  }
 
+    componentDidMount(){
+    return fetch('http://localhost:3000/api/articles')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson,
+      }, function(){
+      });
+    })
+    .catch((error) => {
+      console.error(error)
+    });
+  }
+
+  render(){
+       if(this.state.isLoading){
+      return(
+        <View style={{flex: 2, padding: 20, backgroundColor: 'purple'}}>
+          <ActivityIndicator />
+        </View>
+        )
+    }
     return(
       <SafeAreaView style={styles.myView}>
-        <Text>Hi, welcome home</Text>
+        {
+          this.state.dataSource.map((article) => (
+            <View>
+            <Text>{article.title}</Text>
+            <Image
+            style={{width: 200, height: 200}}
+            source={{uri: article.img}}
+            />
+            <Text>{article.content}</Text>
+            </View>
+          ))
+        }
         </SafeAreaView>
       );
   }
